d55746065fdc34999534ce9e3b6fa7d0118147e1
Soren Hartvig <shartvig@nineconsult.dk>
2014-02-06 16:28:27 +0100

    a lot more

---
+++ src/main/groovy/dk/turnipsoft/discogsparser/parser/impl/processor/ImageFetchProcessor.groovy
@@ -0,0 +1,84 @@
+package dk.turnipsoft.discogsparser.parser.impl.processor
+
+import dk.turnipsoft.discogsparser.api.ListingProcessor
+import dk.turnipsoft.discogsparser.model.Configuration
+import dk.turnipsoft.discogsparser.model.Listing
+import dk.turnipsoft.discogsparser.util.HttpUtil
+
+/**
+ * Created by shartvig on 06/02/14.
+ */
+class ImageFetchProcessor implements ListingProcessor {
+
+    Configuration configuration
+
+    String imageDir = "images"
+
+    HttpUtil httpUtil = new HttpUtil()
+
+    List<String> curls = []
+
+    void initImageDir() {
+        File imageDirectory = new File(configuration.generateDirectory+"/"+imageDir)
+        if (!imageDirectory.exists()) {
+            System.out.println('Making images dir')
+            imageDirectory.mkdir()
+        }
+    }
+
+    private void writeCurlFile() {
+        File f = new File(configuration.generateDirectory+"/getImages.sh")
+
+        PrintWriter pw = new PrintWriter(new FileWriter(f))
+        curls.each { curl->
+            pw.println(curl)
+        }
+
+        pw.close()
+    }
+
+    private boolean fileExists(String filename) {
+        String fullFilename = "$configuration.generateDirectory/$imageDir/$filename"
+        File f = new File(fullFilename)
+        return f.exists()
+    }
+
+    private void fetchFile(String filename, Listing listing) {
+
+        String fullFilename = "$configuration.generateDirectory/$imageDir/$filename"
+
+        if (filename) {
+            System.out.println("fetching image : $filename for listing: $listing.description")
+            try {
+                byte[] imageBytes = httpUtil.getBytesFromUrl(configuration.imageBaseUrl+filename)
+                File f = new File(fullFilename)
+                DataOutputStream dos = new DataOutputStream(new FileOutputStream(f))
+                dos.write(imageBytes)
+                dos.close()
+            } catch (IOException ioe) {
+                System.out.println("Can't fetch image: "+ioe.message)
+                listing.errors.add('Unable to fetch image:'+configuration.imageBaseUrl+filename)
+                curls << "curl $configuration.imageBaseUrl$filename > $fullFilename"
+            }
+        }
+    }
+
+    @Override
+    void init(Configuration configuration) {
+        this.configuration = configuration
+        initImageDir()
+    }
+
+    @Override
+    Object processListing(Listing listing) {
+        if (listing.release.imageFileName && !fileExists(listing.release.imageFileName)) {
+            fetchFile(listing.release.imageFileName, listing)
+        }
+    }
+
+    @Override
+    void endProcessing() {
+        System.out.println("missed out "+curls.size() + " images, so making a curl script for those")
+        writeCurlFile()
+    }
+}
--- src/test/groovy/dk/turnipsoft/discogsparser/util/HttpUtilSpec.groovy
+++ src/test/groovy/dk/turnipsoft/discogsparser/util/HttpUtilSpec.groovy
@@ -21,6 +21,36 @@ class HttpUtilSpec extends Specification {
         result != null
         json != null
     }
+
+    Object 'test getting simple file'() {
+        given:
+        String url = 'http://s.pixogs.com/image/R-150-114443-001.jpg'
+        HttpUtil httpUtil = new HttpUtil()
+        httpUtil.userAgent = 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2'
+
+        when:
+        byte[] result = httpUtil.getBytesFromUrl(url)
+
+        then:
+        result
+        result.size()>0
+
+    }
+
+    Object 'test getting simple file2'() {
+        given:
+        String url = 'http://s.pixogs.com/image/R-150-114443-001.jpg'
+        HttpUtil httpUtil = new HttpUtil()
+        httpUtil.userAgent = 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2'
+
+        when:
+        byte[] result = httpUtil.getBytesFromUrl3(url)
+
+        then:
+        result
+        result.size()>0
+
+    }
 }
 
 
